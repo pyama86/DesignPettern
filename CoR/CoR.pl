92fbe07d76a77b6e8bb9e4bd71ab94bfd8f973e5
ka-yamashita <yamashitakazuhiko@kazuhiko-no-MacBook-Air.local>
2014-07-06 03:16:07 +0900

    CoR Update

--- CoR/CoR.pl
+++ CoR/CoR.pl
@@ -5,16 +5,15 @@ use utf8;
 use strict;
 use warnings;
 use lib ('./');
-use SEARCH::LogProcessor;
 use SEARCH::LogHandler;
 
-#プロセッサーのインスタンス化
-my $p = SEARCH::LogProcessor->new();
 
-#ハンドラーを追加する
-$p->add_handler(SEARCH::LogHandler::Incoming->new());
-$p->add_handler(SEARCH::LogHandler::Virus->new());
-$p->add_handler(SEARCH::LogHandler::Ogo->new());
+my $handlers=SEARCH::LogHandler::Incoming->new(
+	 successor => SEARCH::LogHandler::Virus->new(
+		successor => SEARCH::LogHandler::Ogo->new()
+	)
+);
 
 #処理依頼する
-$p->search_request({type => 'ogo',date => '0101'});
+$handlers->search_request({type => 'ogo',date => '0101'});
+$handlers->search_request({type => 'incoming',date => '0401'});
--- CoR/SEARCH/LogHandler.pm
+++ CoR/SEARCH/LogHandler.pm
@@ -10,6 +10,10 @@ binmode(STDOUT, ":utf8");
 package SEARCH::LogHandler;
 	use Moose::Role;
 		requires 'search_request';
+		has 'successor' => (
+			is		=>	'rw',
+			isa		=>	'SEARCH::LogHandler'
+		);
 	no Moose::Role;
 
 #
@@ -20,13 +24,17 @@ package SEARCH::LogHandler::Incoming;
                 with 'SEARCH::LogHandler';
                 __PACKAGE__->meta->make_immutable();
         no Moose;
-
         sub search_request {
-                my ($class,$request) = @_;
-                return () unless $request->{type} eq 'incm';
-        
-                print "検索タイプは$request->{type}で日付は$request->{date}です\n";
-        }
+                my ($self,$request) = @_;
+                (my $cname =__PACKAGE__)=~ s/.*:://g;
+
+		if($request->{type} =~ /$cname/i){
+                	print "検索タイプは$request->{type}で日付は$request->{date}です\n";
+		}else{
+			$self->successor->search_request($request);
+        	}
+	}
+
 package SEARCH::LogHandler::Virus;
         use Moose;
                 with 'SEARCH::LogHandler';
@@ -34,11 +42,15 @@ package SEARCH::LogHandler::Virus;
         no Moose;
 
         sub search_request {
-                my ($class,$request) = @_;
-                return () unless $request->{type} eq 'virus';
+                my ($self,$request) = @_;
+                (my $cname =__PACKAGE__)=~ s/.*:://g;
 
-                print "検索タイプは$request->{type}で日付は$request->{date}です\n";
-        }
+		if($request->{type} =~ /$cname/i){
+                	print "検索タイプは$request->{type}で日付は$request->{date}です\n";
+		}else{
+			$self->successor->search_request($request);
+        	}
+	}
 package SEARCH::LogHandler::Ogo;
         use Moose;
                 with 'SEARCH::LogHandler';
@@ -46,9 +58,13 @@ package SEARCH::LogHandler::Ogo;
         no Moose;
 
         sub search_request {
-                my ($class,$request) = @_;
-                return () unless $request->{type} eq 'ogo';
+                my ($self,$request) = @_;
+                (my $cname =__PACKAGE__)=~ s/.*:://g;
 
-                print "検索タイプは$request->{type}で日付は$request->{date}です\n";
-        }
+		if($request->{type} =~ /$cname/i){
+                	print "検索タイプは$request->{type}で日付は$request->{date}です\n";
+		}else{
+			$self->successor->search_request($request);
+        	}
+	}
 1;
